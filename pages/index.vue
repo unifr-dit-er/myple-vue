f6cd90bb45c40856c1e6c7b41893330a2415564f
Alrick Deillon <alrick.deillon@unifr.ch>
2023-02-17 14:48:12 +0100

    Center hero on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="hero min-h-screen bg-base-300">
-    <div class="flex mx-4">
+    <div class="flex">
       <img class="w-1/3 hidden md:block" src="~/assets/img/undraw_bibliophile.svg" alt="" />
-      <div class="hero-content mt-12 flex-col">
+      <div class="2xl:w-1/3 hero-content mt-12 flex-col">
           <h1 class="text-7xl font-bold">MyPLE</h1>
           <p class="py-3 w-96 text-center">MyPLE propose 3 types d'aides, en libre accès, sur les outils informatiques pouvant aider à réaliser les tâches de travail du métier d'étudiant·e.</p>
           <IndexStats />
