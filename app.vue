b87902faf86f1e2cd1d3fee01764302ebd54c5f0
Alrick Deillon <alrick.deillon@unifr.ch>
2023-08-04 13:50:20 +0200

    Add html metadata

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useHead({
+  title: "MyPLE",
+  meta: [
+    { name: "MyPLE", content: "MyPLE propose 3 types d'aides, en libre accès, sur les outils informatiques pouvant aider à réaliser les tâches de travail du métier d'étudiant·e." }
+  ]
+})
+</script>
+
 <template>
   <div>
     <Nav />
