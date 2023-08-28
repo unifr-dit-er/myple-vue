8d655b06cbee879670bf89e0a5a432d0cd94c238
Alrick Deillon <alrick.deillon@unifr.ch>
2023-08-28 14:39:18 +0200

    Remove tools description for now

--- pages/tools/index.vue
+++ pages/tools/index.vue
@@ -19,9 +19,6 @@ onMounted(() => {
         <h2 class="text-4xl font-bold leading-normal mb-4">
           <IconToolsLarge class="inline w-8" /> {{ $t('tools') }}
         </h2>
-        <p class="mb-4 text-xl leading-relaxed">
-          {{ $t('tools_description') }}
-        </p>
         <ToolsFilters class="mb-4" />
         <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
           <ToolsItem v-for="tool in store.tools" :key="tool.id" :tool="tool" />
