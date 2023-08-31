ad9b78619155803261c0295a030c89a49be026f3
Alrick Deillon <alrick.deillon@unifr.ch>
2023-08-31 11:43:49 +0200

    Mobile opti : improved menu on small mobiles

--- components/Nav/Mobile.vue
+++ components/Nav/Mobile.vue
@@ -10,7 +10,7 @@ defineProps<{
     <label tabindex="0" class="btn btn-ghost lg:hidden">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
     </label>
-    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box">
+    <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box max-w-xs sm:max-w-none truncate">
       <li>
         <a><IconNotebook /> {{ $t('activities') }}</a>
         <ul class="p-2">
